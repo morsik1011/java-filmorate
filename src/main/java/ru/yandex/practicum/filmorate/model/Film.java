cb591d938c496daf1d4c8df50d5944312be00bfb
Anastasia <N.S.Morsik@yandex.ru>
2025-03-30 17:56:00 +0300

    controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Film {
+    private Long id;
+    @NotEmpty(message = "Название не должно быть null или пустым")
+    private String name;
+    @Size(min = 1, max = 200)
+    private String description;
+    private LocalDate releaseDate;
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    private int duration;
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import lombok.*;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String name;
+    @NotEmpty(message = "Логин не должен быть null или пуст")
+    private String login;
+    @Email(message = "Электронная почта не соответствует формату")
+    @NotEmpty(message = "Электронная почта не может быть пустой")
+    private String email;
+    @Past(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+}
